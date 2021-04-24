63a25f1d79770132a29611c855cdf701ecf069d3
Josué González <38192813@uaz.edu.mx>
2021-04-24 18:08:21 -0500

    Se implementó la cancelación total de una orden y se agregó la restricción de 24 horas para poder realizar una cancelación

--- myshop/orders/urls.py
+++ myshop/orders/urls.py
@@ -12,10 +12,14 @@
 #
 #-------------------------------------------------------------------------
 
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('create/', views.order_create, name='order_create'),
     path('list/',views.order_list,name='order_list'),
+    path('cancel_order/<int:id>',views.cancel_order,name='cancel_order'),
+    path('delete_order_confirm/<int:id>',views.delete_order_confirm,name='delete_order_confirm'),
+    path('order_detail/<int:id>',views.order_detail,name='order_detail'),
+    re_path(r'^order_items/(?P<items_list>\w+)/$',views.cancel_items, name='cancel_items'),
 ]
--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -27,11 +27,16 @@
 #
 #--------------------------------------------------------------------------------------------------
 
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import OrderItem, Order
 from .forms import OrderCreateForm
 from django.core.mail import send_mail
 from cart.cart import Cart
+from datetime import datetime, timedelta, timezone
+from zoneinfo import ZoneInfo
+from django.conf import settings
+import pytz
+import time
 
 
 def order_create(request):
@@ -87,7 +92,68 @@ def send(order_id, cart):
     # Se envía el correo.
     send_mail(subject, body, 'pruebas.jogglez@gmail.com', [order.email], fail_silently=False)
 
+def send_cancel_order(order_id):
+    # Se obtiene la información de la orden.
+    order = Order.objects.get(id=order_id)
+
+    # Se crea el subject del correo.
+    subject = 'Order nr. {}'.format(order.id)
+
+    # Se define el mensaje a enviar.
+    message = 'Dear {},\n\nYou have successfully cancelled an order. The ID of your cancelled order is {}.\n\n\n'.format(order.first_name,order.id)
+    message_part2 = 'The following products were contained in your order: \n\n'
+    mesagges = []
+
+    order_items = []
+    order_items = OrderItem.objects.filter(order=order_id)
+
+    for item in order_items:
+        msg = str(item.quantity) + 'x '+ str(item.product.name) +'  $'+ str(item.get_cost())+ '\n'
+        mesagges.append(msg)
+    
+    message_part3 = ' '.join(mesagges)
+    message_part4 = '\n\n\n Date of cancellation: ' + datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
+    body = message + message_part2 + message_part3 + message_part4
+
+    # Se envía el correo.
+    send_mail(subject, body, 'pruebas.jogglez@gmail.com', [order.email], fail_silently=False)
 
 def order_list(request):
     orders = Order.objects.all()
     return render(request, 'orders/order/list.html', {'orders' : orders})
+
+def cancel_order(request, id):
+    order = Order.objects.get(id=id)
+    flag = True
+    note = "No, I am not sure"
+    todays_date = datetime.now()
+    timezone = pytz.timezone(settings.TIME_ZONE)
+    todays_date_wo = timezone.localize(todays_date)
+    takeaway_hours = todays_date_wo - timedelta(hours=24)
+    
+    local_time = pytz.timezone(settings.TIME_ZONE)
+    order_date = order.created.replace(tzinfo=pytz.utc)
+    order_date = order_date.astimezone(local_time)
+
+    if (takeaway_hours > order_date):
+        flag = False
+        note = "Return to order list"
+ 
+    return render(request, 'orders/order/delete_order_confirm.html', {'order' : order,
+                                                                      'flag' : flag,
+                                                                      'note' : note})
+def delete_order_confirm(request, id):
+    order = Order.objects.get(id=id)
+    send_cancel_order(order.id)
+    order.delete()
+    return redirect('order_list')
+
+def order_detail(request, id):
+    order = Order.objects.get(id=id)
+    order_items = []
+    order_items = OrderItem.objects.filter(order=id)
+    return render(request, 'orders/order/items_list.html', {'order' : order,
+                                                            'order_items' : order_items})
+
+def cancel_items(request):
+    pass
